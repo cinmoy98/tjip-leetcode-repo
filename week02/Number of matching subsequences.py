8c701b9f198f725bb39c6210410476836b2f2b8c
cinmoy98 <cinmoy98@gmail.com>
2022-04-23 13:16:10 +0600

    Number Of matching Ocurrence Revisit

--- week02/Number of matching subsequences.py
+++ week02/Number of matching subsequences.py
@@ -1,3 +1,50 @@
+# Problem Link : https://leetcode.com/problems/number-of-matching-subsequences/
+# Problem Number : 792
+# Problem Tag : String
+# Author : Cinmoy Das (github : https://github.com/cinmoy98)
+# Date : 23/04/2022
+
+# Approach 2 (Occurrence count and binary search)(Used editorial😥)
+# Time complexity --> O(MlogN)
+# Space Complexity --> O(N)
+################################################################
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        ocur_indx = defaultdict(list)
+        for i,c in enumerate(s):
+            ocur_indx[c].append(i)
+        
+        def issubseq(word):
+            min_pos = -1
+            for char in word:
+                if ocur_indx[char]:
+                    indx = bisect.bisect_right(ocur_indx[char], min_pos)
+                    if indx == len(ocur_indx[char]):
+                        return False
+                    min_pos = ocur_indx[char][indx]
+                else:
+                    return False
+            return True
+        
+        
+        
+        cnt = 0
+        for word in words:
+            if issubseq(word):
+                cnt+=1
+        return cnt
+
+
+
+
+
+
+
+#Approach 1(Occurrence,no bin search)
+# Time complexity --> O(M*N)
+# Space Complexity --> O(N)
+#####################################################################################
 from collections import defaultdict
 s = "bacinmoydass"
 words = ["aass","cidaa","cindoy","moyda","ass", "aioy", "cibd"]
